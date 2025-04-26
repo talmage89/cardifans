9fb05141be23dde2ca97c0da7f0b9731d33312bc
talmage89 <talmage.bergeson@gmail.com>
2025-04-25 19:35:18 -0600

    Fix Vercel Preset configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { vercelPreset } from "@vercel/react-router/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), vercelPreset(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
